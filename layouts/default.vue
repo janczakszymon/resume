204f60144751edfc66c204a2ae9dc3ef62d343aa
szymon <s.janczak04@gmail.com>
2024-08-15 20:57:48 +0200

    added footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,20 @@
 <template>
   <UContainer>
-    <slot/>
+    <main>
+      <slot/>
+    </main>
+    <footer>
+      <UDivider class="my-3"></UDivider>
+      <div class="flex justify-between">
+        <span>Created by Szymon Janczak &#169; {{ currentYear }}</span>
+        <span>Theme inspired by
+          <NuxtLink to="https://www.2020.works/" external target="_blank" class="underline">2020.works</NuxtLink>
+        </span>
+      </div>
+    </footer>
   </UContainer>
 </template>
 
 <script setup lang="ts">
+const currentYear = new Date().getFullYear();
 </script>
