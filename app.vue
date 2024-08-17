ec8f11a90122dd240135947de594f6389494624f
szymon <s.janczak04@gmail.com>
2024-08-17 16:28:06 +0200

    added meta and logos

--- app.vue
+++ app.vue
@@ -6,9 +6,25 @@
 
 <script setup lang="ts">
 useHead({
+	title: 'Szymon Janczak',
+	htmlAttrs: {
+		lang: 'pl',
+	},
 	bodyAttrs: {
 		class: 'bg-zinc-100 dark:bg-slate-900',
 	},
+	meta: [
+		{
+			name: 'description',
+			content:
+          'Resume website',
+		},
+		{
+			name: 'keywords',
+			content:
+          'Resume',
+		},
+	]
 });
 </script>
 
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -29,4 +29,7 @@ export default defineNuxtConfig({
 			},
 		},
 	},
+	colorMode: {
+		preference: 'light'
+	}
 });
