3f3371e32a095d8a7d71d1ed92bc5f0abf762221
szymon <s.janczak04@gmail.com>
2024-08-19 18:09:28 +0200

    fixed mikrus logo, moved footer data to nav

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,24 +2,5 @@
 	<UContainer>
 		<NavbarComponent />
 		<slot />
-		<footer class="mb-3">
-			<UDivider class="my-3" />
-			Szymon Janczak &#169; {{ currentYear }}
-		</footer>
 	</UContainer>
 </template>
-
-<script setup lang="ts">
-const currentYear = new Date().getFullYear();
-</script>
-
-<style scoped lang="scss">
-footer {
-  color: $gray;
-  font-size: .8rem;
-
-  @screen md {
-    font-size: 1rem;
-  }
-}
-</style>
